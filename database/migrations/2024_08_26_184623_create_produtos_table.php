ffcf963f12a2451004faec703f8c51beaf9d2d2c
Tatiane Guzman <126589385+tat1546@users.noreply.github.com>
2024-08-26 16:17:29 -0300

    .
    .

--- database/migrations/2024_08_26_184623_create_produtos_table.php
+++ database/migrations/2024_08_26_184623_create_produtos_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
             $table->string('nome',50);
             $table->text('descricao');
             $table->integer('peso');
-            $table->float('preco', 8, 2); // 8 dígitos no total, 2 após o ponto decimal
+            $table->float('preco', 8, 2); 
             $table->integer('estoque');
-            $table->timestamps(); // Adiciona created_at e updated_at
+            $table->timestamps(); 
         });
     }
 
